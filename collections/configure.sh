e048e12e30a4db67637f2843383c32daf92459ed
pavithra-mahamani <42983325+pavithra-mahamani@users.noreply.github.com>
2020-04-14 05:15:54 -0700

    connection

--- collections/configure.sh
+++ collections/configure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo hi
-java -jar /tmp/javaclient.jar -i $CLUSTER -u $USERNAME -p $PASSWORD -b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -pr $PR -l $L -dsn $DSN -dpx $DPX -dsx $DSX -dt $DT -o $O
+java -jar /tmp/javaclient.jar -i $CLUSTER -b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -pr $PR -l $L -dsn $DSN -dpx $DPX -dsx $DSX -dt $DT -o $O
