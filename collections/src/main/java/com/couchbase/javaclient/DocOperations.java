e6f3d3e58932165a3a02f58265f48485b429741e
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2020-05-14 20:20:21 -0700

    username not mandatory

--- collections/src/main/java/com/couchbase/javaclient/DocOperations.java
+++ collections/src/main/java/com/couchbase/javaclient/DocOperations.java
@@ -33,10 +33,8 @@ public class DocOperations {
 				.defaultHelp(true).description("Standalone SDK Client");
 		// Connection params
 		parser.addArgument("-i", "--cluster").required(true).help("Couchbase cluster address");
-		parser.addArgument("-u", "--username").required(false).setDefault("Administrator")
-				.help("Username of Couchbase user");
-		parser.addArgument("-p", "--password").required(false).setDefault("password")
-				.help("Password of Couchbase user");
+		parser.addArgument("-u", "--username").setDefault("Administrator").help("Username of Couchbase user");
+		parser.addArgument("-p", "--password").setDefault("password").help("Password of Couchbase user");
 		parser.addArgument("-b", "--bucket").setDefault("default").help("Name of existing Couchbase bucket");
 		parser.addArgument("-s", "--scope").setDefault("_default").help("Name of existing scope");
 		parser.addArgument("-c", "--collection").setDefault("default").help("Name of existing collection");
