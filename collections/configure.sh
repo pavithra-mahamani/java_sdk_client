43091563ecea5ef5b7e4ecc2861c69a433afa736
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2020-05-14 21:10:04 -0700

    argparse fix

--- collections/configure.sh
+++ collections/configure.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo $CLUSTER
 java -jar /tmp/javaclient.jar -i $CLUSTER -u $USERNAME -p $PASSWORD -b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -l $L -dsn $DSN -dpx $DPX -dt $DT -o $O
 
