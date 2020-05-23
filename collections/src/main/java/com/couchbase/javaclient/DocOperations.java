8db8c3107e16166e793965a465062f5976c4509a
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2020-05-22 18:32:57 -0700

    username fix

--- collections/src/main/java/com/couchbase/javaclient/DocOperations.java
+++ collections/src/main/java/com/couchbase/javaclient/DocOperations.java
@@ -29,47 +29,50 @@ import com.github.javafaker.Faker;
 
 public class DocOperations {
 	public static void main(String[] args) {
-		//ArgumentParser parser = ArgumentParsers.newFor("Couchbase Java SDK Client For Collections").build()
-		//		.defaultHelp(true).description("Standalone SDK Client");
-		final ArgumentParser parser = ArgumentParsers.newArgumentParser("Main", true);
+		 ArgumentParser parser = ArgumentParsers.newFor("Couchbase Java SDK Client For
+		 Collections").build()
+		 .defaultHelp(true).description("Standalone SDK Client");
+		//final ArgumentParser parser = ArgumentParsers.newArgumentParser("Main", true);
 		// Connection params
 		parser.addArgument("-i", "--cluster").required(true).help("Couchbase cluster address");
-		//parser.addArgument("-u", "--username").setDefault("Administrator").help("Username of Couchbase user");
-		//parser.addArgument("-p", "--password").setDefault("password").help("Password of Couchbase user");
+		parser.addArgument("-u", "--username").setDefault("Administrator").help("Username of Couchbase user");
+		parser.addArgument("-p", "--password").setDefault("password").help("Password of Couchbase user");
 		parser.addArgument("-b", "--bucket").setDefault("default").help("Name of existing Couchbase bucket");
 		parser.addArgument("-s", "--scope").setDefault("_default").help("Name of existing scope");
 		parser.addArgument("-c", "--collection").setDefault("default").help("Name of existing collection");
 
-        // Operation params
-        parser.addArgument("-n", "--num_ops").type(Integer.class).setDefault(1000).help("Number of operations");
-        parser.addArgument("-pc", "--percent_create").type(Integer.class).setDefault(100)
-                        .help("Percentage of creates out of num_ops");
-        parser.addArgument("-pu", "--percent_update").type(Integer.class).setDefault(100)
-                        .help("Percentage of updates out of num_ops");
-        parser.addArgument("-pd", "--percent_delete").type(Integer.class).setDefault(10)
-                        .help("Percentage of deletes out of num_ops");
-        parser.addArgument("-pr", "--percent_read").type(Integer.class).setDefault(0)
-                        .help("Percentage of reads out of num_ops");
-        parser.addArgument("-l", "--load_pattern").choices("uniform", "sparse", "dense").setDefault("uniform")
-                        .help("uniform: load all collections with percent_create docs, "
-                                        + "sparse: load all collections with maximum of percent_create docs"
-                                        + "dense: load all collections with minimum of percent_create docs");
+		// Operation params
+		parser.addArgument("-n", "--num_ops").type(Integer.class).setDefault(1000)
+		.help("Number of operations");
+		parser.addArgument("-pc", "--percent_create").type(Integer.class).setDefault(100)
+		.help("Percentage of creates out of num_ops");
+		parser.addArgument("-pu", "--percent_update").type(Integer.class).setDefault(0)
+		.help("Percentage of updates out of num_ops");
+		parser.addArgument("-pd", "--percent_delete").type(Integer.class).setDefault(0)
+		.help("Percentage of deletes out of num_ops");
+		parser.addArgument("-pr", "--percent_read").type(Integer.class).setDefault(0)
+		.help("Percentage of reads out of num_ops");
+		parser.addArgument("-l", "--load_pattern").choices("uniform", "sparse", "dense")
+		.setDefault("uniform")
+		.help("uniform: load all collections with percent_create docs, "
+				+ "sparse: load all collections with maximum of percent_create docs"
+				+ "dense: load all collections with minimum of percent_create docs");
 
-        // Doc params
-        parser.addArgument("-dsn", "--start_seq_num").type(Integer.class).setDefault(1)
-                        .help("Doc id start sequence number");
-        parser.addArgument("-dpx", "--prefix").setDefault("doc_").help("Doc id prefix");
-        parser.addArgument("-dsx", "--suffix").setDefault("").help("Doc id suffix");
-        parser.addArgument("-dt", "--template").setDefault("Person").help("JSON document template");
-        parser.addArgument("-de", "--expiry").setDefault(0).help("Document expiry in seconds");
-        parser.addArgument("-ds", "--size").setDefault(500).help("Document size in bytes");
+		// Doc params
+		parser.addArgument("-dsn", "--start_seq_num").type(Integer.class).setDefault(1)
+		.help("Doc id start sequence number");
+		parser.addArgument("-dpx", "--prefix").setDefault("doc_").help("Doc id prefix");
+		parser.addArgument("-dsx", "--suffix").setDefault("").help("Doc id suffix");
+		parser.addArgument("-dt", "--template").setDefault("Person").help("JSON document template");
+		parser.addArgument("-de", "--expiry").setDefault(0).help("Document expiry in seconds");
+		parser.addArgument("-ds", "--size").setDefault(500).help("Document size in bytes");
 
-        // Output params
-        parser.addArgument("-o", "--output").setDefault("info").help("Output detail level");
+		// Output params
+		parser.addArgument("-o", "--output").setDefault("info").help("Output detail level");
 
 		try {
 			Namespace ns = parser.parseArgs(args);
-			//System.out.println(parser.parseArgs(args));
+			// System.out.println(parser.parseArgs(args));
 			run(ns);
 		} catch (ArgumentParserException e) {
 			parser.handleError(e);
@@ -78,8 +81,8 @@ public class DocOperations {
 
 	private static void run(Namespace ns) {
 		String clusterName = ns.getString("cluster");
-		String username = "Administrator";
-		String password = "password";
+		String username = ns.getString("username");
+		String password = ns.getString("password");
 		String bucketName = ns.getString("bucket");
 		String scopeName = ns.getString("scope");
 		String collectionName = ns.getString("collection");
