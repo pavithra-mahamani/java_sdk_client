bc41fcdb88b6984f4dc781cd4395908833206752
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2020-05-27 07:15:34 -0700

    exec format error

--- collections/configure.sh
+++ collections/configure.sh
@@ -1,4 +1,5 @@
-#/bin/bash
+#!/bin/bash
+
 echo java -jar /tmp/javaclient.jar -i $CLUSTER -u $USERNAME -p $PASSWORD $-b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -l $L -dsn $DSN -dpx $DPX -dt $DT -o $O
 java -jar /tmp/javaclient.jar -i $CLUSTER -u $USERNAME -p $PASSWORD -b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -l $L -dsn $DSN -dpx $DPX -dt $DT -o $O
 
