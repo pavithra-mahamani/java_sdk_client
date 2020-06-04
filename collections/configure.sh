a1760bfd491b7e10d37bf69d233712ee0735f99d
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2020-06-03 18:57:58 -0700

    fixed typo

--- collections/configure.sh
+++ collections/configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo java -jar /tmp/javaclient.jar -i $CLUSTER -u $USERNAME -p $PASSWORD $-b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -l $L -dsn $DSN -dpx $DPX -dt $DT -o $O
+echo java -jar /tmp/javaclient.jar -i $CLUSTER -u $USERNAME -p $PASSWORD -b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -l $L -dsn $DSN -dpx $DPX -dt $DT -o $O
 java -jar /tmp/javaclient.jar -i $CLUSTER -u $USERNAME -p $PASSWORD -b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -l $L -dsn $DSN -dpx $DPX -dt $DT -o $O
 
